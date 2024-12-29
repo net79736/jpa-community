bc721d970464896664039725ed7ecb5d827f30f4
leejongwook2 <34103253+leejongwook2@users.noreply.github.com>
2024-12-30 06:38:23 +0900

    gradle build 에러 나서 classes = JpaCommunityApplication.class 명시

--- src/test/java/com/leep/jpacommunity/JpaCommunityApplicationTests.java
+++ src/test/java/com/leep/jpacommunity/JpaCommunityApplicationTests.java
@@ -1,9 +1,10 @@
 package com.leep.jpacommunity;
 
+import com.jpacommunity.JpaCommunityApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = JpaCommunityApplication.class)
 class JpaCommunityApplicationTests {
 
     @Test
